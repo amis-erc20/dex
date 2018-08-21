4b147191fbb7c75f92ffec04d658abff99ba352f
amisolution <siriusgem1@gmail.com>
2018-08-21 06:18:13 +0000

    Create Marketplace.js

---
+++ static/js/pages/trade/components/Marketplace.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import jss from 'react-jss'
+import MarketplaceChooser from './MarketplaceChooser'
+import TokenChooser from './TokenChooser'
+import Panel from 'components/Panel'
+
+const decorate = jss({
+  root: {
+    display: 'flex',
+    flex: 'none'
+  },
+  wrapper: {
+    marginRight: 30
+  }
+})
+
+const Marketplace = ({classes}) =>
+  <Panel className={classes.root}>
+    <div className={classes.wrapper}>
+      <MarketplaceChooser />
+    </div>
+    <div className={classes.wrapper}>
+      <TokenChooser />
+    </div>
+  </Panel>
+
+export default decorate(Marketplace)
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/trade/components/Marketplace.js
