56d43018fa09c07c228277631377470227927e68
amisolution <siriusgem1@gmail.com>
2018-08-21 06:29:03 +0000

    Create store.js

---
+++ static/js/pages/store.js
@@ -0,0 +1,18 @@
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import rootReducer from './modules'
+import {send} from './ws'
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunk.withExtraArgument({send})
+  )
+)
+
+export default store
+
+
+
+// WEBPACK FOOTER //
+// ./src/store.js
