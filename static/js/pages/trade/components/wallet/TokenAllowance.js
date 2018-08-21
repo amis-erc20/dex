147345e3fedfca50d9b7d689a1e39b6000694bcd
amisolution <siriusgem1@gmail.com>
2018-08-21 06:07:51 +0000

    Create TokenAllowance.js

---
+++ static/js/pages/trade/components/wallet/TokenAllowance.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import jss from 'react-jss'
+import Switch from '@material-ui/core/Switch'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {setUnlimitedTokenAllowance, setZeroTokenAllowance, loadTokenAllowance} from 'modules/index'
+
+const connector = connect(
+  (state, ownProps) => ({
+    allowance: state.tokenAllowances[ownProps.token.symbol]
+  }),
+  dispatch => bindActionCreators({setUnlimitedTokenAllowance, setZeroTokenAllowance, loadTokenAllowance}, dispatch)
+)
+
+const decorate = jss({
+  root: {
+    marginTop: -8,
+    marginBottom: -8
+  }
+})
+
+class TokenAllowance extends React.Component {
+  componentDidMount () {
+    this.props.loadTokenAllowance(this.props.token)
+  }
+
+  handleChange = e => {
+    const {token} = this.props
+    const {checked} = e.target
+
+    if (checked) {
+      this.props.setUnlimitedTokenAllowance(token)
+    } else {
+      this.props.setZeroTokenAllowance(token)
+    }
+  }
+
+  render () {
+    const {allowance, classes} = this.props
+    return (
+      <Switch
+        className={classes.root}
+        value='allowance'
+        checked={allowance || false}
+        onChange={this.handleChange}
+      />
+    )
+  }
+}
+
+export default connector(decorate(TokenAllowance))
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/trade/components/wallet/TokenAllowance.js
