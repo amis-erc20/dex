f5bc2c7e4989e7b2d5d28e7e8401ad70adf579d1
amisolution <siriusgem1@gmail.com>
2018-08-21 06:29:38 +0000

    Create theme.js

---
+++ static/js/pages/theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import grey from '@material-ui/core/colors/grey'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: grey
+  }
+})
+
+export default theme
+
+
+
+// WEBPACK FOOTER //
+// ./src/theme.js
