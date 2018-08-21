1a587119fac917891ab14830a8c6d7d89e2ef84b
amisolution <siriusgem1@gmail.com>
2018-08-21 06:13:41 +0000

    Create UnwrapWethForm.js

---
+++ static/js/pages/trade/components/wallet/UnwrapWethForm.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import jss from 'react-jss'
+import TextField from '@material-ui/core/TextField'
+import SmartButton from 'material-ui-smart-button'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {unwrapWeth} from 'modules/index'
+
+const connector = connect(
+  null,
+  dispatch => bindActionCreators({unwrapWeth}, dispatch)
+)
+
+const decorate = jss({
+  root: {
+    display: 'flex',
+    marginBottom: 2
+  },
+  button: {
+    marginLeft: 10
+  }
+})
+
+class UnwrapWethForm extends React.Component {
+  state = {
+    value: ''
+  }
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleClick = async () => {
+    const amount = parseFloat(this.state.value, 10)
+
+    await this.props.unwrapWeth(amount)
+
+    this.setState({
+      value: ''
+    })
+  }
+
+  render () {
+    const {classes} = this.props
+    const {value} = this.state
+    return (
+      <div className={classes.root}>
+        <TextField value={value} onChange={this.handleChange} />
+        <SmartButton variant='raised' onClick={this.handleClick} className={classes.button}>Unwrap</SmartButton>
+      </div>
+    )
+  }
+}
+
+export default connector(decorate(UnwrapWethForm))
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/trade/components/wallet/UnwrapWethForm.js
