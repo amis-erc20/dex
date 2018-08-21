8c7de9e59b53679bf9ee35c245bf5c639c5ec684
amisolution <siriusgem1@gmail.com>
2018-08-21 06:06:04 +0000

    Create EthToken.js

---
+++ static/js/pages/trade/components/wallet/EthToken.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import jss from 'react-jss'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {loadEthBalance} from 'modules/index'
+import WrapEthForm from './WrapEthForm'
+import TokenHeader from './TokenHeader'
+import TokenBalance from './TokenBalance'
+
+const connector = connect(
+  state => ({
+    ethBalance: state.ethBalance
+  }),
+  dispatch => bindActionCreators({loadEthBalance}, dispatch)
+)
+
+const decorate = jss({
+  root: {},
+  content: {
+    display: 'flex',
+    alignItems: 'center',
+    height: 32
+  }
+})
+
+class EthBalance extends React.Component {
+  render () {
+    const {classes, ethBalance} = this.props
+    return (
+      <div className={classes.root}>
+        <TokenHeader symbol='ETH' name='Ethereum' />
+        <div className={classes.content}>
+          <TokenBalance balance={ethBalance} load={this.props.loadEthBalance} />
+        </div>
+        <WrapEthForm />
+      </div>
+    )
+  }
+}
+
+export default connector(decorate(EthBalance))
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/trade/components/wallet/EthToken.js
