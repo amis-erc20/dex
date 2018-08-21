0ff21799d809cde581a15df0dab3e2d304d24c27
amisolution <siriusgem1@gmail.com>
2018-08-21 06:03:39 +0000

    Create createTrComponent.js

---
+++ static/js/pages/trade/components/orderbook/createTrComponent.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import cx from 'classnames'
+import SpreadRow from './SpreadRow'
+
+const createTrComponent = DefaultRowComponent =>
+  ({spread, className, highlightClassName, ...props}) => {
+    if (spread) {
+      return <SpreadRow spread={spread} />
+    }
+    return (
+      <DefaultRowComponent
+        {...props}
+        className={cx(className, highlightClassName)}
+      />
+    )
+  }
+
+export default createTrComponent
+
+
+
+// WEBPACK FOOTER //
+// ./src/pages/trade/components/orderbook/createTrComponent.js
